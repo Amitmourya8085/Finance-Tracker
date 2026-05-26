371ded13d32843f2c740e40d7deaa8b6c864b7a1
Amitmourya8085 <amitmourya8085@gmail.com>
2026-05-26 15:14:19 +0530

    Created TransactionResponseDTO added and updated Transaction service

--- finance-tracker/src/main/java/com/amit8085/finance_tracker/DTO/TransactionResponseDTO.java
+++ finance-tracker/src/main/java/com/amit8085/finance_tracker/DTO/TransactionResponseDTO.java
@@ -1,5 +1,47 @@
 package com.amit8085.finance_tracker.DTO;
 
+import com.amit8085.finance_tracker.Entity.TransactionType;
+import com.amit8085.finance_tracker.Entity.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
 public class TransactionResponseDTO {
 
+    private Long id;
+    private double amount;
+    private TransactionType type;
+    private String category;
+    private LocalDate date;
+    private User user;
+
+
+
+    public TransactionResponseDTO(double amount, TransactionType type, String category, LocalDate date, User user) {
+        this.amount = amount;
+        this.type = type;
+        this.category = category;
+        this.date = date;
+        this.user = user;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
--- finance-tracker/src/main/java/com/amit8085/finance_tracker/Service/TransactionService.java
+++ finance-tracker/src/main/java/com/amit8085/finance_tracker/Service/TransactionService.java
@@ -1,15 +1,81 @@
 package com.amit8085.finance_tracker.Service;
 
+import com.amit8085.finance_tracker.DTO.TransactionRequestDTO;
+import com.amit8085.finance_tracker.DTO.TransactionResponseDTO;
+import com.amit8085.finance_tracker.Entity.Transaction;
+import com.amit8085.finance_tracker.Entity.TransactionType;
+import com.amit8085.finance_tracker.Entity.User;
 import com.amit8085.finance_tracker.Repository.TransactionRepository;
+import com.amit8085.finance_tracker.Repository.UserRepository;
+import org.apache.catalina.valves.rewrite.InternalRewriteMap;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransactionService {
     private final TransactionRepository trepo;
+    private final UserRepository urepo;
 
-    public TransactionService(TransactionRepository trepo){
+    public TransactionService(TransactionRepository trepo, UserRepository urepo){
         this.trepo =trepo;
+        this.urepo = urepo;
+    }
+    //ADD TRANSACTION
+    public TransactionResponseDTO addTransaction(TransactionRequestDTO dto){
+       //validate Transaction Type;
+        TransactionType type;
+        try {
+            type=TransactionType.valueOf(dto.getType().toUpperCase());
+        }catch(Exception e){
+            throw new RuntimeException("Invalid type. Use INCOME or EXPENSE");
+        }
+        //check for user;
+        User user = urepo.findById(dto.getUserId()).orElseThrow(() -> new RuntimeException("User not found"));
+        //Create transaction
+        Transaction transaction = new Transaction();
+        transaction.setAmount(dto.getAmount());
+        transaction.setType(type);
+        transaction.setCategory(dto.getCategory());
+        transaction.setDate(dto.getDate());
+        transaction.setUser(user);
+
+        Transaction saved = trepo.save(transaction);
+        return new TransactionResponseDTO(
+                saved.getAmount(),
+                saved.getType(),
+                saved.getCategory(),
+                saved.getDate(),
+                saved.getUser()
+        );
+
     }
 
+    //  GET USER TRANSACTIONS
+    public List<Transaction> getUserTransactions(Long userId) {
+
+        if (!urepo.existsById(userId)) {
+            throw new RuntimeException("User not found");
+        }
+
+        return trepo.findByUserId(userId);
+    }
+    //  CALCULATE BALANCE (Improved)
+    public double calculateBalance(Long userId) {
+
+        List<Transaction> transactions = trepo.findByUserId(userId);
+
+        double balance = 0;
+
+        for (Transaction t : transactions) {
+            if (t.getType() == TransactionType.INCOME) {
+                balance += t.getAmount();
+            } else {
+                balance -= t.getAmount();
+            }
+        }
+
+        return balance;
+    }
 
 }
